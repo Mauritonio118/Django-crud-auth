5100d761ee8cfb5e1f25b3399b5d86a93ed2861e
mauro.ojeda.f <mauro.ojeda.f@gmail.com>
2023-04-08 18:17:00 -0400

    Registro de usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'singup.html', {
-        'form': UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def singup(request):
+
+    if request.method =='GET':
+        return render(request, 'singup.html', {
+            "form": UserCreationForm,
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #Register User
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse("<h1>User created</h1>")
+            except:
+                return HttpResponse("<h1>Username already exist</h1>")
+        return HttpResponse("<h1>Password Fail</h1>")
