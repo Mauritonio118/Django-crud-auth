039c7653e50e6d9f6ea7f7241a36ab5ba975dfe2
mauro.ojeda.f <mauro.ojeda.f@gmail.com>
2023-04-08 23:45:59 -0400

    Logout y navegacion

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.http import HttpResponse
-
+from django.db import IntegrityError
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def singup(request):
 
-    if request.method =='GET':
+    if request.method == 'GET':
         return render(request, 'singup.html', {
-            "form": UserCreationForm,
+            "form": UserCreationForm
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                #Register User
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                # Register User
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse("<h1>User created</h1>")
-            except:
-                return HttpResponse("<h1>Username already exist</h1>")
-        return HttpResponse("<h1>Password Fail</h1>")
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'singup.html', {
+                    "form": UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        return render(request, 'singup.html', {
+            "form": UserCreationForm,
+            'error': 'Password do not macht'
+        })
+
+def tasks(request):
+    return render(request, 'tasks.html')
